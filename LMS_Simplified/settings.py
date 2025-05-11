428ddeaff339aa16a75617a33791659f3d360061
Saad <waqarsaad76@gmail.com>
2025-05-11 18:57:06 +0500

    added CORS

--- LMS_Simplified/settings.py
+++ LMS_Simplified/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'LMS',
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
 
 
     'django.contrib.admin',
@@ -57,6 +58,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -84,6 +89,32 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'LMS_Simplified.wsgi.application'
+
+
+# added cors:
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT'
+]
+
+
 
 
 # Database
